42193c013ddf4eca9fc4c5fe455efe8e70ba1f82
Sitael <beru666@live.fr>
2020-03-19 14:38:11 +0100

    git push -u into terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -17,3 +17,4 @@ git merge <nom de la branche> -m "<message du commit>" # même chose que la lign
 git remote                                         # liste les dépôts distants de notreprojet git
 git remote add <nom du dépôt distant> <adresse du dépôt> # ajoute un nouveau dépôt distant (origin dans 99% des cas)
 git push <nom du dépôt> <branche distante>         #envoie la branche courante sur la branche distante du dépôt choisi
+git push -u <nom du dépôt> <branche distante>      # idem mais paramètre la branche local pour coresspondre à la branche distante
