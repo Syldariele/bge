35173f7c79e63b4bd70627f2d1efe7993260d669
Sitael <beru666@live.fr>
2020-03-19 09:38:10 +0100

    add terminal.sh

---
+++ terminal.sh
@@ -0,0 +1,11 @@
+# DIVERS
+cd <nom du fichier>                               # SE DEPLACER
+node <nom du fichier>                             # LANCER UN SCRIPT NODE
+
+#GIT
+git init                                          # INITIALISER UN PROJET GIT
+git add <nom du fichier/dossier>                  # AJOUTER LES MODIFICATIONS AU PROCHAIN COMMIT
+git commit -m "<message à attacher au commit>"    #SAUVEGARDER LES MODIFICATION PRECEDEMENT AJOUTEES
+git status                                        # AFFICHE L'ETAT DE MON PROJET GGIT 0 CET INSTANT
+git log                                           #AFFICHE L'HISTORIQUE DES COMMITS
+
